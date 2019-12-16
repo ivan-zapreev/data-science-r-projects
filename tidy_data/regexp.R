45c85c9d4edc55abaeb956983706c17ce438f112
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2019-12-16 23:03:34 +0100

    working on the course chapters and assignments

---
+++ tidy_data/regexp.R
@@ -0,0 +1,70 @@
+# load raw heights data and inspect
+library(dslabs)
+data(reported_heights)
+class(reported_heights$height)
+
+# convert to numeric, inspect, count NAs
+x <- as.numeric(reported_heights$height)
+head(x)
+sum(is.na(x))
+
+# keep only entries that result in NAs
+reported_heights %>% mutate(new_height = as.numeric(height)) %>%
+  filter(is.na(new_height)) %>% 
+  head(n=10)
+
+# calculate cutoffs that cover 99.999% of human population.
+alpha <- 1/10^6
+qnorm(1-alpha/2, 69.1, 2.9)
+qnorm(alpha/2, 63.7, 2.7)
+
+# keep only entries that either result in NAs or are outside the plausible range of heights
+not_inches <- function(x, smallest = 50, tallest = 84){
+  inches <- suppressWarnings(as.numeric(x))
+  ind <- is.na(inches) | inches < smallest | inches > tallest
+  ind
+}
+
+# number of problematic entries
+problems <- reported_heights %>% 
+  filter(not_inches(height)) %>%
+  .$height
+length(problems)
+
+# 10 examples of x'y or x'y" or x'y\"
+pattern <- "^\\d\\s*'\\s*\\d{1,2}\\.*\\d*'*\"*$"
+str_subset(problems, pattern) %>% head(n=10) %>% cat
+
+# 10 examples of x.y or x,y
+pattern <- "^[4-6]\\s*[\\.|,]\\s*([0-9]|10|11)$"
+str_subset(problems, pattern) %>% head(n=10) %>% cat
+
+# 10 examples of entries in cm rather than inches
+ind <- which(between(suppressWarnings(as.numeric(problems))/2.54, 54, 81) )
+ind <- ind[!is.na(ind)]
+problems[ind] %>% head(n=10) %>% cat
+
+# load stringr through tidyverse
+library(tidyverse)
+
+# detect whether a comma is present
+pattern <- ","
+str_detect(murders_raw$total, pattern) 
+
+# show the subset of strings including "cm"
+str_subset(reported_heights$height, "cm")
+
+# use the "or" symbol inside a regex (|)
+yes <- c("180 cm", "70 inches")
+no <- c("180", "70''")
+s <- c(yes, no)
+str_detect(s, "cm") | str_detect(s, "inches")
+str_detect(s, "cm|inches")
+
+# highlight the first occurrence of a pattern
+pattern<-"cm"
+str_view(s, pattern)
+
+# highlight all instances of a pattern
+pattern <-"'"
+str_view_all(s, pattern)
