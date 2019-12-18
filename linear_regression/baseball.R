335e5ea615aac01c2d5f491e6a9423493f08d743
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2019-12-18 10:13:25 +0100

    Course Section 1: Introduction to Regression 1.1: Baseball as a Motivating Example

---
+++ linear_regression/baseball.R
@@ -0,0 +1,61 @@
+# The goal of a baseball game is to score more runs (points) than the other team.
+# Each team has 9 batters who have an opportunity to hit a ball with a bat in a predetermined order. 
+# Each time a batter has an opportunity to bat, we call it a plate appearance (PA).
+# The PA ends with a binary outcome: the batter either makes an out (failure) and returns to the bench or the batter doesn’t (success) and can run around the bases, and potentially score a run (reach all 4 bases).
+# We are simplifying a bit, but there are five ways a batter can succeed (not make an out):
+#   
+# 1. Bases on balls (BB): the pitcher fails to throw the ball through a predefined area considered to be hittable (the strike zone), so the batter is permitted to go to first base.
+# 2. Single: the batter hits the ball and gets to first base.
+# 3. Double (2B): the batter hits the ball and gets to second base.
+# 4. Triple (3B): the batter hits the ball and gets to third base.
+# 5. Home Run (HR): the batter hits the ball and goes all the way home and scores a run.
+# 
+# Historically, the batting average has been considered the most important offensive statistic. To define this average, we define a hit (H) and an at bat (AB). Singles, doubles, triples and home runs are hits. The fifth way to be successful, a walk (BB), is not a hit. An AB is the number of times you either get a hit or make an out; BBs are excluded. The batting average is simply H/AB and is considered the main measure of a success rate.
+
+library(Lahman)
+library(tidyverse)
+library(dslabs)
+ds_theme_set()
+
+#Scatterplot of the relationship between HRs and wins
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_per_game = HR / G, R_per_game = R / G) %>%
+  ggplot(aes(HR_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+#Scatterplot of the relationship between stolen bases and wins
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(SB_per_game = SB / G, R_per_game = R / G) %>%
+  ggplot(aes(SB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+#Scatterplot of the relationship between bases on balls and runs
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_per_game = BB / G, R_per_game = R / G) %>%
+  ggplot(aes(BB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+#Scatterplot of the relationship between at bats and runs
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(AB_per_game = AB / G, R_per_game = R / G) %>%
+  ggplot(aes(AB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
+
+#Scatterplot of the relationship between runs and at bats
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(AB_per_game = AB / G, R_per_game = R / G) %>%
+  ggplot(aes(R_per_game, AB_per_game)) + 
+  geom_point(alpha = 0.5)
+
+#Scatterplot of the relationship between number of wins per game and fielding errors 
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(W_per_game = W / G, E_per_game = E / G) %>%
+  ggplot(aes(W_per_game, E_per_game)) + 
+  geom_point(alpha = 0.5)
+
+#Scatterplot of the relationship between triples (X3B) per game and doubles (X2B) per game 
+Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(X3B_per_game = X3B / G, X2B_per_game = X2B / G) %>%
+  ggplot(aes(X3B_per_game, X2B_per_game)) + 
+  geom_point(alpha = 0.5)
+
