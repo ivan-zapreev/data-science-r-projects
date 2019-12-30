4414cac6ab07f614c0fe7435a2d3e09a4d171a31
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2019-12-30 16:40:54 +0100

    Working on the initial project script

--- capstone/movielens_project.R
+++ capstone/movielens_project.R
@@ -1,37 +1,81 @@
 ####################################################################
-# 
-#
+# Section to define and load needed packages
 ####################################################################
 
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 
+####################################################################
+# Section to define global variables
+####################################################################
+
+MOVIELENS_DATA_SET_NAME <- "MovieLens 10M dataset"
+MOVIELENS_DATA_SET_SITE_URL <- "https://grouplens.org/datasets/movielens/10m/"
+MOVIELENS_DATA_SET_FILE_URL <- "http://files.grouplens.org/datasets/movielens/ml-10m.zip"
+MOVIELENS_DATA_FILE_NAME <- "movielens.dat"
+RATINGS_DAT_FILE_NAME <- "ml-10M100K/ratings.dat"
+MOVIES_DAT_FILE_NAME <- "ml-10M100K/movies.dat"
+VALIDATION_SET_PROPORTION <- 0.1
+MOVIELENS_REPORT_DATA_FILE_NAME <- "movielens_report.dat"
+
+####################################################################
+# Section to define helper functions
+####################################################################
+
 #--------------------------------------------------------------------
-# The function derived from the project initialization instructions
-# to create edx set, validation set. It returns a data frame with the
+#This is a helper function which allows to remove the objects from the
+#environment in case they are present. This function is obtained from
+#the internet: 
+# https://stackoverflow.com/questions/7172568/write-a-function-to-remove-object-if-it-exists
+#--------------------------------------------------------------------
+ifrm <- function(obj, env = globalenv()) {
+  obj <- deparse(substitute(obj))
+  if(exists(obj, envir = env)) {
+    rm(list = obj, envir = env)
+  }
+}
+
+#--------------------------------------------------------------------
+# This function derived from the project initialization instructions
+# to create edx set, validation set. It returns a list with the
 # two corresponding entries:
 #   edx - storing the edx set, meant for training
 #   validation - storing the validation set, meant for final RMSE evaluation
 #
 # Note: Running this function could take a couple of minutes
 #--------------------------------------------------------------------
-create_exd_and_vaidation_sets <- function() {
-  # Download the MovieLens 10M dataset:
+create_movielens_sets <- function() {
+  # Download the MovieLens 10M dataset, if not already:
   # https://grouplens.org/datasets/movielens/10m/
   # http://files.grouplens.org/datasets/movielens/ml-10m.zip
-  dl <- tempfile()
-  download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
+  
+  if(!file.exists(RATINGS_DAT_FILE_NAME) || !file.exists(MOVIES_DAT_FILE_NAME)){
+    dl <- tempfile()
+    cat("Downloading data file:", MOVIELENS_DATA_SET_FILE_URL,
+        "into a temporary file:", dl, "\n")
+    download.file(MOVIELENS_DATA_SET_FILE_URL, dl)
+    
+    cat("Unzipping the files:", RATINGS_DAT_FILE_NAME, "and ", MOVIES_DAT_FILE_NAME, "\n")
+    unzip(dl, RATINGS_DAT_FILE_NAME)
+    unzip(dl, MOVIES_DAT_FILE_NAME)
+  } else {
+    cat("The ", RATINGS_DAT_FILE_NAME, "and", MOVIES_DAT_FILE_NAME,
+        "are locally present, no need to re-download from", MOVIELENS_DATA_SET_FILE_URL, "\n")
+  }
   
   #Read the movie ratings from the downloaded file
-  ratings <- fread(text = gsub("::", "\t", readLines(unzip(dl, "ml-10M100K/ratings.dat"))),
+  cat("Reading", RATINGS_DAT_FILE_NAME, "data from the temporary file\n")
+  ratings <- fread(text = gsub("::", "\t", readLines(RATINGS_DAT_FILE_NAME)),
                    col.names = c("userId", "movieId", "rating", "timestamp"))
   
   #Read the movies from the downloaded file
-  movies <- str_split_fixed(readLines(unzip(dl, "ml-10M100K/movies.dat")), "\\::", 3)
+  cat("Reading", MOVIES_DAT_FILE_NAME, "data from the temporary file\n")
+  movies <- str_split_fixed(readLines(MOVIES_DAT_FILE_NAME), "\\::", 3)
   colnames(movies) <- c("movieId", "title", "genres")
   
   #Convert the movies into a data frame
+  cat("Pre-processing and joining data from", MOVIES_DAT_FILE_NAME, " and ", RATINGS_DAT_FILE_NAME, "\n")
   movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(levels(movieId))[movieId],
                                              title = as.character(title),
                                              genres = as.character(genres))
@@ -44,7 +88,11 @@ create_exd_and_vaidation_sets <- function() {
   # if using R 3.5 or earlier, use `set.seed(1)` instead
 
   #Validation set will be about 10% of MovieLens data
-  test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+  cat("Splitting data with", (1-VALIDATION_SET_PROPORTION)*100, 
+      "% for the edx (train) set, and", VALIDATION_SET_PROPORTION*100,
+      "% for the validation (test) set\n")
+  test_index <- createDataPartition(y = movielens$rating, times = 1, 
+                                    p = VALIDATION_SET_PROPORTION, list = FALSE)
   
   #Make the edx (training) data to be the 90% of the data set
   edx <- movielens[-test_index,]
@@ -52,25 +100,95 @@ create_exd_and_vaidation_sets <- function() {
   temp <- movielens[test_index,]
   
   #Make sure userId and movieId in validation set are also in edx set.
+  cat("Make sure that the validation set only contains users and movies from the edx set\n");
   validation <- temp %>% 
     semi_join(edx, by = "movieId") %>%
     semi_join(edx, by = "userId")
   
   # Add rows removed from validation set back into edx set
+  cat("Move the excluded events back to the edx set\n");
   removed <- anti_join(temp, validation)
   edx <- rbind(edx, removed)
   
   #Removed the non-needed temporary data
-  rm(dl, ratings, movies, test_index, temp, movielens, removed)
+  ifrm(dl)
+  rm(ratings, movies, test_index, temp, movielens, removed)
   
-  data.frame(edx=edx, validation=validation)
+  cat("Finished preparing the data, creating the final data frame\n");
+  list(edx=edx, validation=validation)
 }
 
-#----------------------------------------------------------------
-# This is the main part of the cript that will be
-#calling the utility functions from above
-
-#Initialize the data
-dat <- create_exd_and_vaidation_sets()
-
+#--------------------------------------------------------------------
+# This function is reponsible for getting the movielens train and test data
+# If the data is present in the local MOVIELENS_DATA_FILE_NAME file then
+# the data is simplu loaded from there, if not then it is re-generated and
+# is then also stored into the MOVIELENS_DATA_FILE_NAME for re-use.
+# It returns a list with the two corresponding entries:
+#   edx - storing the edx set, meant for training
+#   validation - storing the validation set, meant for final RMSE evaluation
 #
+# Note: Running this function could take a couple of minutes
+#--------------------------------------------------------------------
+get_movielens_data <- function() {
+  #Check if the file exists then load the data from the file, 
+  #otherwise re-create the data and also save it to the file
+  cat("Checkin if the Movielens data is stored in:", MOVIELENS_DATA_FILE_NAME,"\n")
+  if(!file.exists(MOVIELENS_DATA_FILE_NAME)){
+    cat("The data is not stored in", MOVIELENS_DATA_FILE_NAME, "start re-generating\n")
+    dat <- create_movielens_sets()
+    cat("The data is re-generated, storing it into:", MOVIELENS_DATA_FILE_NAME, "\n")
+    save(dat, file = MOVIELENS_DATA_FILE_NAME)
+  } else {
+    cat("The data is stored in", MOVIELENS_DATA_FILE_NAME, "and will be loaded\n")
+    dat <- load(MOVIELENS_DATA_FILE_NAME)
+  }
+  dat
+}
+
+#--------------------------------------------------------------------
+# This function creates the initial report to be filled
+#--------------------------------------------------------------------
+init_report_data <- function() {
+  tibble(
+    name = MOVIELENS_DATA_SET_NAME,
+    site_url = MOVIELENS_DATA_SET_SITE_URL,
+    data_file_url = MOVIELENS_DATA_SET_FILE_URL,
+    valid_set_prop = VALIDATION_SET_PROPORTION,
+    exd = data.frame(
+      num_events = nrow(dat$edx),
+      num_predictors = ncol(dat$edx),
+      num_movies = length(unique(edx$movieId)),
+      num_users = length(unique(edx$userId))
+    ),
+    validation = data.frame(
+      num_events = nrow(dat$validation),
+      num_predictors = ncol(dat$validation)
+    ) 
+  )
+}
+
+#--------------------------------------------------------------------
+# This function stores the final report into the 
+#     MOVIELENS_REPORT_DATA_FILE_NAME
+# file to be used later from the movielens_report.Rmd script
+#--------------------------------------------------------------------
+
+store_report_data <- function(report_data) {
+  save(report_data, file = MOVIELENS_REPORT_DATA_FILE_NAME)
+}
+
+####################################################################
+# Section to define the main part of the cript that will be
+# calling the utility functions from above and performing the
+# main sequence
+####################################################################
+
+#Load the movielens data
+movielens_data <- get_movielens_data()
+
+#Initialize the report data frame thay will be storing all
+#the required information for the report to be generated
+report_data <- init_report_data()
+
+#Store the report into the file to be used from the movielens_report.Rmd
+store_report_data(report_data)
